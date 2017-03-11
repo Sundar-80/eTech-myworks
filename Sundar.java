70e766044528db465ab1fa8c2aaa024c6751d7b7
Sundar-80 <sundargalaxy668@gmail.com>
2017-03-11 17:25:46 -0600

    Updated

--- Sundar.java
+++ Sundar.java
@@ -1,6 +1,8 @@
 public class Sundar
 {
    public static void main(String[] args){
-    System.out.println("Sundar Murthy Chandran ... !!!");
+    for(int i=0;i<10;i++){
+	   System.out.println("Sundar Murthy Chandran ... !!!");
+    }
    }
  }
